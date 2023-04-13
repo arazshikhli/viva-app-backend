23f484481ee2009021d04708a866f2ae1764cfa8
arazshikhli <arazaraz777@gmail.com>
2023-04-13 12:47:29 +0400

    last

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
-import {editAdminValidation} from'./validations/auth.js'
+import {editAdminValidation,loginValidation} from'./validations/auth.js'
 import { validationResult } from 'express-validator';
 import AdminModel from './Models/Admin.js';
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
+
 
 dotenv.config()
 const URL=process.env.MONGODB_URI
@@ -19,31 +20,89 @@ mongoose.connect(`mongodb+srv://viva:viva123@cluster0.qvys0rj.mongodb.net/viva-a
 })
 const app=express();
 app.use(express.json())
-const posts=[{name:"araz",surname:"shikhli"},{name:"araz",surname:"shikhli"}]
-app.get('/auth/posts',async(req,res)=>{
- res.send({
-    message:"Hello world"
- })
-})
+// const posts=[{name:"araz",surname:"shikhli"},{name:"araz",surname:"shikhli"}]
+// app.get('/auth/posts',async(req,res)=>{
+//  res.send({
+//     message:"Hello world"
+//  })
+// })
 app.post('/auth/editadmin',editAdminValidation,async (req,res)=>{
-   const errors=validationResult(req);
-   if(!errors.isEmpty()){
-    return res.status(400).json(errors.array())
-   }
+try {
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+     return res.status(400).json(errors.array())
+    }
+ 
+    const password=req.body.password;
+    const checkPassword=password[0];
+    const firstLetterCapitalize=checkPassword.toUpperCase();
 
-   const password=req.body.password;
-   const salt=await bcrypt.genSalt(10);
-   const passwordHash=await bcrypt.hash(password,salt);
+    if(checkPassword!=firstLetterCapitalize){
+        res.json({
+            message:"Первая буква с большой"
+        })
+    }
+   
+    const salt=await bcrypt.genSalt(10);
+    const hash=await bcrypt.hash(password,salt);
+ 
+    const doc=new AdminModel({
+     email:req.body.email,
+     fullName:req.body.fullName,
+     avatarUrl:req.body.avatarUrl,
+     passwordHash:hash,
+    });
+ 
+    const admin=await doc.save()
+    const token=jwt.sign({
+        _id:admin._id,
+    },'secret123',
+    {expiresIn:"30d"})
 
-   const doc=new AdminModel({
-    email:req.body.email,
-    fullName:req.body.fullName,
-    avatarUrl:req.body.avatarUrl,
-    passwordHash,
-   });
+    const {passwordHash,...adminData}=admin._doc
+    res.json({
+        ...adminData,
+        token
+    })
+    
+} catch (error) {
+    console.log(error)
+    res.json({
+       message:"Error" 
+    })
+}
+})
+app.post('/auth/login',async (req,res)=>{
+    try {
+        const admin=await AdminModel.findOne({email:req.body.email});
+        if(!admin){
+          return  res.status(404).json({
+                message:"неверный ящик почты"
+            })
+        }
+        const isValidPass=await bcrypt.compare(req.body.password,admin._doc.passwordHash);
+        if (!isValidPass){
+            return  res.json({
+                message:"неверный пароль"
+            }) 
+        }
+        const token=jwt.sign({
+            _id:admin._id,
+        },'secret123',
+        {expiresIn:"30d"})
 
-   const admin=await doc.save()
-   res.json(admin)
+        const {passwordHash,...adminData}=admin._doc
+        res.json({
+            ...adminData,
+            token
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:"Не удалось войти в систему"
+        })
+    }
 })
 
 app.listen(4444,(err)=>{
@@ -51,4 +110,5 @@ app.listen(4444,(err)=>{
         return console.log(er)
     }
     console.log('Server Ok')
+    
 })
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,14 @@
 import {body} from 'express-validator'
 
+
 export const editAdminValidation=[
     body('email',"Неверный формат почты").isEmail(),
-    body('password',"Пароль слишком короткий").isLength({min:5}),
+    body('password',"Пароль слишком короткий").isLength({min:8}),
     body('fullName',"Имя не менее 5-и символов").isLength({min:5}),
     body('avatarUrl').optional().isURL(),
+];
+
+export const loginValidation=[
+    body('email',"Неверный формат почты").isEmail(),
+    body('password',"Пароль слишком короткий").isLength({min:8}),
 ]
